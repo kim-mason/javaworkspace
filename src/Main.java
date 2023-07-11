723974941888be60befccc9f61e843e0466dfa29
kim-mason <mlsd1504@gmail.com>
2023-07-11 16:57:09 +0900

    java git test

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String s1 = String.valueOf(93);
+        s1 = Integer.toString(32);
+        String s2 = String.valueOf(2);
+        s2 = Integer.toString(22);
+        System.out.println(s1 + s2);
+
+
+    }
+}
